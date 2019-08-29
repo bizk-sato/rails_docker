6f3eba8046b4573ca46e7dae3880032db97f5e5e
hashimoto <gorogoro1985@yahoo.ne.jp>
2019-08-30 00:45:58 +0900

    全てプッシュします

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user=User.find_by(id:params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user=User.new(
+      name:params[:name],
+      age:params[:age],
+      gender:params[:gender],
+      description:params[:description]
+    )
+    @user.save
+    redirect_to("/users/index")
+  end
+
+  def edit
+    @user=User.find_by(id:params[:id])
+  end
+
+  def update
+    @user=User.find_by(id:params[:id])
+    @user.name=params[:name]
+    @user.age=params[:age]
+    @user.gender=params[:gender]
+    @user.description=params[:description]
+    @user.save
+    redirect_to("/users/index")
+  end
+
+  def destroy
+    @user=User.find-by(id:params[:id])
+    @user.destroy
+    redirect_to("users/index")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get "users", to: "users#index"
+  #get "users/:id", to: "users#show"
+  resources :users # 7つのアクションを作成してくれる
+  
 end
